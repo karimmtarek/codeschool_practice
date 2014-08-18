2cb5593e515693a61c40025f579bc1a5f5a9c569
Karim Tarek <karimmtarek@gmail.com>
2014-08-18 06:29:25 -0400

    Level 1, done

--- javascript_road_trip_part_3/source/javascripts/js_functions_expressions.js
+++ javascript_road_trip_part_3/source/javascripts/js_functions_expressions.js
@@ -81,5 +81,82 @@ var func_4 = function(num){
   return num % 4;
 };
 
-var puzzlers = [func_1, func_2, func_3, func_4];
+// var puzzlers = [func_1, func_2, func_3, func_4];
 
+/**/
+
+var parkRides = [
+  ["Birch Bumpers", 40],
+  ["Pines Plunge", 55],
+  ["Cedar Coaster", 20],
+  ["Ferris Wheel of First", 90]
+];
+
+var fastPassQueue = ["Cedar Coaster", "Pines Plunge", "Birch Bumpers", "Pines Plunge"];
+
+var userPick = "Birch Bumpers";
+
+function buildTicket(allRides, passRides, pick){
+  if(passRides[0] === pick){
+    var pass = passRides.shift();
+    return function () {
+      alert("Quick! You've got a Fast Pass to " + pass + "!");
+    };
+  } else {
+    for(var i = 0; i<allRides.length; i++){
+      if(allRides[i][0] === pick){
+        return function(){
+          alert("A ticket is printing for " + pick + "!\n" + "Your wait time is about " + allRides[i][1] + " minutes.");
+        };
+      }
+    }
+  }
+}
+
+var getTicket = buildTicket(parkRides, fastPassQueue, userPick);
+
+/**/
+
+function adventureSelector ( userChoice ){
+  if(userChoice === 1){
+    return function(){
+      alert("You've selected the Vines of Doom!\nHope you have a swingin' time.");
+    };
+  } else if (userChoice === 2){
+    return function(){
+      alert("Looks like you want the Lake of Despair!\nWatch out for crocs. And I ain't talkin' about shoes.");
+    };
+  } else {
+    return function(){
+      alert("The Caves of Catastrophe, really?\nAlright, well....nice knowing you.");
+    };
+  }
+}
+
+/**/
+
+var puzzlers = [
+  function ( a ) { return 8*a - 10; },
+  function ( a ) { return (a-3) * (a-3) * (a-3); },
+  function ( a ) { return a * a + 4; },
+  function ( a ) { return a % 5; }
+];
+
+var start = 2;
+var puzzlersLength = puzzlers.length;
+
+
+var applyAndEmpty = function( input, queue ) {
+
+  var length = queue.length;
+  for(var i = 0; i<length; i++){
+    input = queue.shift()(input);
+    console.log(input);
+  }
+  return input;
+};
+
+// alert(applyAndEmpty(2, puzzlers));
+// alert(puzzlers[3](9));
+// alert(puzzlers[1](3));
+alert(puzzlers[puzzlers[1](3)](puzzlers[3](9)));
